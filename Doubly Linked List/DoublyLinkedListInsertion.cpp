1b75996609c974731f8b27e32ce21312c581effa
mahi000007 <mahendran1482004@gmail.com>
2024-01-18 06:24:37 +0530

    feat: add insertion in Doubly linked list

---
+++ Doubly Linked List/DoublyLinkedListInsertion.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void insertHead(Node *&head, int data)
+{
+    Node *temp = new Node(data);
+    temp->next = head;
+    head->prev = temp;
+    head = temp;
+    cout << "Inserted Head Successfully.." << endl;
+}
+
+void insertTail(Node *&tail, int data)
+{
+    Node *temp = new Node(data);
+    tail->next = temp;
+    temp->prev = tail;
+    tail = temp;
+    cout << "Inserted Tail Successfully.." << endl;
+}
+
+void insertIntoPosition(Node *&head, Node *&tail, int position, int data)
+{
+    if (position == 1)
+    {
+        insertHead(head, data);
+        return;
+    }
+    Node *temp = head;
+    int count = 1;
+    while (count < position - 1)
+    {
+        temp = temp->next;
+        count++;
+    }
+    if ((temp->next)->next == NULL)
+    {
+        insertTail(tail, data);
+        return;
+    }
+    Node *newNode = new Node(data);
+    newNode->prev = temp;
+    newNode->next = temp->next;
+    (temp->next)->prev = newNode;
+    temp->next = newNode;
+    cout << "Inserted at " << position << " Position Successfully.." << endl;
+}
+
+void printFun(Node *head)
+{
+    if (head == NULL)
+    {
+        cout << "The Linked List is Empty" << endl;
+        return;
+    }
+    else
+    {
+        while (head != NULL)
+        {
+            cout << head->data << '\t';
+            head = head->next;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    Node *node1 = new Node(15);
+    head = node1;
+    tail = node1;
+    insertHead(head, 5);
+    printFun(head);
+    insertTail(tail, 20);
+    printFun(head);
+    insertIntoPosition(head, tail, 2, 10);
+    printFun(head);
+    return 0;
+}
